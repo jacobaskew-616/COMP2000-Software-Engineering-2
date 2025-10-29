1e8c839c6aa4aeaef595c78bcb1c0139a351ad08
jacob <jacob.askew@students.plymouth.ac.uk>
2025-10-29 16:09:23 +0000

    Created the staff portal page

---
+++ app/src/main/java/com/example/comp2000/StaffPortalActivity.java
@@ -0,0 +1,12 @@
+package com.example.comp2000;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StaffPortalActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_staff_portal);
+    }
+}
